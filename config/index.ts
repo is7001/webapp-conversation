ca996de8dedb4332ded649e238e885a50cc2ab42
is7001 <is7001@gmail.com>
2023-08-21 17:05:30 +0900

    Update index.ts
    APP_ID, API_KEY, API_URL update

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = 'd298547f-97f2-46f7-95b8-e820f0fc03a3'
+export const API_KEY = 'd298547f-97f2-46f7-95b8-e820f0fc03a3'
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
